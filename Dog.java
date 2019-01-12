2b9ba332fd46353c38d74ba2a0f3b631fb9e39c4
NikitaJTelisara <nikitatelisara@gmail.com>
2019-01-12 17:12:16 -0600

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,5 +1,10 @@
 public class Dog extends Animal {
     public Dog(){
+        // 1.invokes the parent constructor, 
+       // 2. not mandatory
+       //3. If a constructor does not explicitly invoke a superclass constructor, 
+        //the Java compiler automatically inserts a call to the no-argument constructor of the superclass. 
+        //If the superclass does not have a no-argument constructor, you will get a compile-time error.      
         super();
         System.out.println("Its a dog");
     }
